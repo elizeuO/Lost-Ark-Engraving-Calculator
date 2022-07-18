547a0a4eb04b42dbfc59165eb33b4d1d011e5f10
Elizeu <elizeu_os@hotmail.com>
2022-07-18 18:05:32 -0300

    Add components to app

--- src/components/App/builder.js
+++ src/components/App/builder.js
@@ -1,14 +1,34 @@
 import React from "react";
+import EngravingItem from "../EngravingItem/builder";
 
+const App = () => {
 
-const App = ()=> {
-  const message = 'teste';
-  
+  const amountEarring = 2;
+  const amountRing = 2;
+  const amountEngravingBook= 1;
+  const amountNecklace = 1;
+  const amountAbilityStone= 1;
+
   return (
-    <h1 >
-        {message}
-    </h1>
+    <section className="c-section">
+
+      <div className="c-container">
+        <h1>Engraving Calculator</h1>
+
+        <div className="l-flex l-flex--center l-flex--wrap l-flex--negative">
+          
+          <article className="l__col-6">
+          <EngravingItem/>
+          </article>
+
+          <article className="l__col-6">
+          </article>
+
+        </div>
+
+      </div>
+    </section>
   );
-}
+};
 
 export default App;
